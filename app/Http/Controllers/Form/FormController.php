9add643eaac55729a14cdf7cd9638a79d56abd4d
JoaoGAR <jgarchanjo@gmail.com>
2024-11-13 14:43:48 -0300

    estrutura de submit formulario dinamico

---
+++ app/Http/Controllers/Form/FormController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Form;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
+
+use App\Http\Services\FormService;
+
+class FormController extends Controller
+{
+    protected $formService;
+
+    public function __construct(FormService $formService)
+    {
+        $this->formService = $formService;
+    }
+
+    public function getForm(Request $request)
+    {
+        $formId = $request->formId;
+        $form = $this->formService->getForm($formId);
+
+        if (!$form) {
+            return response()->json(['error' => 'Formulário não encontrado'], 404);
+        }
+
+        return Response::make($form, 200, ['Content-Type' => 'json']);
+    }
+
+    public function submitData(Request $request, $formId)
+    {
+        $form = $this->formService->getForm($formId);
+        if (!$form) {
+            return response()->json(['error' => 'Formulário não encontrado'], 404);
+        }
+
+        $validationRules = [];
+
+        foreach ($form['fields'] as $field) {
+            $rules = [];
+            if ($field['required']) {
+                $rules[] = 'required';
+            }
+
+            switch ($field['type']) {
+                case 'text':
+                    $rules[] = 'string';
+                    break;
+                case 'number':
+                    $rules[] = 'integer';
+                    break;
+                case 'select':
+                    $rules[] = 'in:' . implode(',', $field['choices']);
+                    break;
+            }
+
+            $validationRules[$field['id']] = $rules;
+            $validator = Validator::make($request->input('fields'), $validationRules);
+
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], 400);
+            }
+
+            $validatedData = $validator->validated();
+
+            return response()->json([
+                'message' => 'Dados salvos com sucesso!',
+                'data' => $validatedData
+            ], 201);
+        }
+
+        return Response::make($form, 200, ['Content-Type' => 'json']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Form\FormController;
 
-Route::get('/forms', [FormController::class, 'generateFormHtml']);
+Route::get('/forms', [FormController::class, 'getForm']);
+
+Route::prefix('formularios')->group(function () {
+    Route::post('/{formId}/preenchimentos', [FormController::class, 'submitData']);
+});
