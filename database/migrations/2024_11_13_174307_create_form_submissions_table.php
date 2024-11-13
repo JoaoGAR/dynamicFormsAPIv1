4b5692603212161e6cc8c81e16867aa859781cac
JoaoGAR <jgarchanjo@gmail.com>
2024-11-13 14:44:32 -0300

    Migration

---
+++ database/migrations/2024_11_13_174307_create_form_submissions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormSubmissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('form_submissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('form_id');
+            $table->timestamp('submitted_at')->useCurrent();
+            $table->json('fields');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('form_submissions');
+    }
+}
